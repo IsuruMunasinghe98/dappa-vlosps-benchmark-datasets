5768c0b997ec7dc4cea1811ece3302bf34cdee5f
Isuru_106221 <isuru.munasinghe@axiatadigitallabs.com>
2025-07-14 12:55:28 +0530

    add files

--- path-smoothing/bezier_curve.py
+++
@@ -1,6 +0,0 @@
-def bezier_curve(P0, P1, P2, num_points=10):
-    curve = []
-    for t in np.linspace(0, 1, num_points):
-        point = (1 - t)**2 * np.array(P0) + 2 * (1 - t) * t * np.array(P1) + t**2 * np.array(P2)
-        curve.append(point.tolist())
-    return curve
